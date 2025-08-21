d3b8f827af743ee19a1538634132976166cb1fef
imam <imamalbana123@gmail.com>
2025-08-21 13:44:23 +0700

    feat(mahasiswa): add GET all, GET by ID, and POST endpoints
    - Add GET /mahasiswa to fetch all mahasiswa
    - Add GET /mahasiswa/:id to fetch mahasiswa by ID
    - Add POST /mahasiswa to insert new mahasiswa

--- index.js
+++ index.js
@@ -1,9 +1,46 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const db = require("./src/db");
+const bodyParser = require("body-parser");
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello Imam");
+});
+
+app.get("/mahasiswa", (req, res) => {
+  const sql = "SELECT * FROM mahasiswa";
+  db.query(sql, (err, results) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: "Gagal mengambil data" });
+    }
+    res.json(results);
+  });
+});
+
+app.get("/mahasiswa/:id", (req, res) => {
+  const id = req.params.id;
+  const sql = `SELECT * FROM mahasiswa WHERE id = ${id}`;
+  db.query(sql, (err, results) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: "Gagal mengambil data" });
+    }
+    res.json(results);
+  });
+});
+
+app.post("/mahasiswa", (req, res) => {
+  const { nama, jurusan } = req.body;
+  console.log(req.body);
+  const sql = `INSERT INTO mahasiswa (nama,jurusan) VALUES (${nama}', '${jurusan})`;
+  db.query(sql, (err, result) => {
+    console.log(result);
+    if (err) throw err;
+    res.send(result);
+  });
 });
 
 app.listen(port, () => {
